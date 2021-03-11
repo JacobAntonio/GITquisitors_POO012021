6df4e80ebeb4e089682029f74ee034d10dafad35
Guillermo-Galdamez <00084320@uca.edu.sv>
2021-03-10 21:37:34 -0600

    Intro a GIT Modificación #1

---
+++ Resta/Resta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al programa que resta dos numeros");  
+             
+            int a=0, b=0;
+
+            Console.WriteLine ("Ingrese primer numero: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine ("Ingrese segundo numero: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine ($"Restando {a} - {b} que es igual a { AddNumbers (a, b) }");
+        }
+        public static int AddNumbers(int a, int b)
+        {
+          return a - b;
+        }
+
+    }
+    
+    
+}
